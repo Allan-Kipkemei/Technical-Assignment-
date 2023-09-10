b8a2a76c1264e747e80e7d82d173e188a47f2159
Allan Kiplagat <allankiplagatkipkemei@gmail.com>
2023-09-10 12:58:51 +0300

    deleted conflicts

--- api/space/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.Model):
-    username = models.CharField( 'USERNAME' max_length=240)
-    email=models.EmailField()
-    password = models.  
